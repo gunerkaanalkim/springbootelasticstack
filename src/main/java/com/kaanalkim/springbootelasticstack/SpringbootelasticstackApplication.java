3a93dc4c9314daf471bfa6b8e36e4c555f1f9877
kaanalkim <kaan.alkim@felislabs.com>
2020-01-29 21:06:22 +0300

    first commit

---
+++ src/main/java/com/kaanalkim/springbootelasticstack/SpringbootelasticstackApplication.java
@@ -0,0 +1,13 @@
+package com.kaanalkim.springbootelasticstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootelasticstackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootelasticstackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kaanalkim/springbootelasticstack/SpringbootelasticstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaanalkim.springbootelasticstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootelasticstackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
